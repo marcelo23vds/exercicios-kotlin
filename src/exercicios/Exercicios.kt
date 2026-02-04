efe839429950c4c6d0e0b40d73c999e729493b3b
MARCELO VIEIRA DA SILVA <25132416@dsn127m1249387.cfp127aluno.local>
2026-02-04 10:33:42 -0300

    part2

--- src/exercicios/Exercicios.kt
+++ src/exercicios/Exercicios.kt
@@ -2,6 +2,7 @@ package exercicios
 
 class Exercicios {
 
+    //PARTE 1
 
     // 1
     fun verificarImparPar(numero: Int){
@@ -11,7 +12,6 @@ class Exercicios {
             println("O numero $numero é impar");
         }
     }
-
     // 2
     fun verificarPositivoNegativo(numero: Int){
         if (numero < 0) {
@@ -22,18 +22,16 @@ class Exercicios {
             println("O numero $numero é zero");
         }
     }
-
     // 3
     fun verificarIdade(idade: Int){
-        when (idade) {
-            in 0..12      -> println("A idade é $idade, então é criança");
-            in 13 .. 17   -> println("A idade é $idade, então é adolescente");
-            in 18 .. 59   -> println("A idade é $idade, então é adulto");
-            in 60 .. 140  -> println("A idade é $idade, então é idoso");
+        when {
+            idade in 0..12      -> println("A idade é $idade, então é criança");
+            idade in 13 .. 17   -> println("A idade é $idade, então é adolescente");
+            idade in 18 .. 59   -> println("A idade é $idade, então é adulto");
+            idade > 60  -> println("A idade é $idade, então é idoso");
             else -> println("Idade $idade invalida");
         }
     }
-
     // 4
     fun exibirTabuada(numero: Int){
         var total = 0;
@@ -42,20 +40,18 @@ class Exercicios {
             println("$numero X $i = $total");
         }
     }
-
     // 5
     fun somaValoresArray(listaNumeros: Array<Int>){
         var i = 0;
         var totalSoma = 0;
 
         while (i < listaNumeros.size){
-            totalSoma = totalSoma + listaNumeros[i];
+            totalSoma += listaNumeros[i];
             i++;
         }
 
         println("Total da soma dos valores informados: $totalSoma");
     }
-
     // 6
     fun maiorValorArray(listaNumeros: Array<Int>){
 
@@ -72,41 +68,36 @@ class Exercicios {
 
         println("O maior valor informado foi: $maior");
     }
-
     // 7
     fun contagemRegressiva(numero: Int){
         var i = numero;
         while (i >= 0){
             println(i);
-            i = i - 1;
+            i -= 1;
         }
     }
-
     // 8
     fun somaParesArray(listaNumeros: Array<Int>){
         var i = 0;
         var totalSoma = 0;
         while (i < listaNumeros.size){
             if (listaNumeros[i] % 2 == 0){
-                totalSoma = totalSoma + listaNumeros[i];
+                totalSoma += listaNumeros[i];
             }
             i++;
         }
         println("Valor total da soma dos números PARES: $totalSoma");
     }
-
     // 9
     fun dobrarValor(numero: Int){
         val valorDobrado = numero * 2;
         println("O dobro de $numero é: $valorDobrado");
     }
-
     // 10
     fun calcularMediaAluno(nota1: Int , nota2: Int, nota3: Int){
         val notaMedia = (nota1 + nota2 + nota3) / 3;
         println("A média do aluno é $notaMedia");
     }
-
     // 11
     fun converterTemperatura (temperaturaCelsius: Double){
         val temperaturaFahrenheit = (temperaturaCelsius * 1.8) + 32;
--- src/exercicios/Main.kt
+++ src/exercicios/Main.kt
@@ -2,38 +2,57 @@ package exercicios
 
 fun main() {
 
-    //1
-    //Exercicios().verificarImparPar(12);
+    //AMBIENTAÇÃO PARTE 1 - 02/02/2026
 
-    //2
-    //Exercicios().verificarPositivoNegativo(9);
+//    //1
+//    Exercicios().verificarImparPar(12);
+//    //2
+//    Exercicios().verificarPositivoNegativo(9);
+//    // 3
+//    Exercicios().verificarIdade(33);
+//    // 4
+//    Exercicios().exibirTabuada(14);
+//    // 5
+//    Exercicios().somaValoresArray(arrayOf(1,2,3,4));
+//    // 6
+//    Exercicios().maiorValorArray(arrayOf(2,9,80,6));
+//    // 7
+//    Exercicios().contagemRegressiva(10);
+//    // 8
+//    Exercicios().somaParesArray(arrayOf(1,24,3,4));
+//    // 9
+//    Exercicios().dobrarValor(28);
+//    // 10
+//    Exercicios().calcularMediaAluno(8, 9, 10);
+//    // 11
+//    Exercicios().converterTemperatura(20.0);
 
-    // 3
-    //Exercicios().verificarIdade(33);
 
-    // 4
-    //Exercicios().exibirTabuada(14);
+    //AMBIENTAÇÃO PARTE 2 - 04/02/2026
 
-    // 5
-    //Exercicios().somaValoresArray(arrayOf(1,2,3,4));
+    //utilização do FILTER (Seleciona alguns elementos -> retorna uma collection)
+//    val numeros = listOf(1,2,3,4,5,6);
+//    val pares = numeros.filter { it % 2 == 0 };
+//    println(pares);
 
-    // 6
-    //Exercicios().maiorValorArray(arrayOf(2,9,80,6));
+    //utilização do MAP (Transforma cada elemento, ou seja todos -> retorna uma collection)
+//    val numeros = listOf(1,2,3,4,5,6);
+//    val dobrados = numeros.map { it * 2 }
+//    println(dobrados);
 
-    // 7
-    //Exercicios().contagemRegressiva(10);
+    //utilização do REDUCE (Combina tudo em um único valor -> retorna 1 unico valor/item)
+//    val range = 1 .. 6;
+//    val numeros = range.toList();
+//    val soma = numeros.reduce {acumulador, numero -> acumulador + numero}
+//    println(soma);
 
-    // 8
-    //Exercicios().somaParesArray(arrayOf(1,24,3,4));
-
-    // 9
-    //Exercicios().dobrarValor(28);
-
-    // 10
-    //Exercicios().calcularMediaAluno(8, 9, 10);
-
-    // 11
-    //Exercicios().converterTemperatura(20.0);
-
+    //encadeamento
+//    val numeros = listOf(1,2,3,4,5);
+//    val somaDobroPares = numeros
+//        .filter { numero -> numero % 2 == 0 }
+//        .map {numero -> numero * 2}
+//        .reduce { acumulador, numero -> acumulador + numero }
+//
+//    println(somaDobroPares);
 
 }  
